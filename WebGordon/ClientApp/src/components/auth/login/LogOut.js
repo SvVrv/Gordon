5ea94a80cf2f7e213d03dbdc78d7d6181f35fc6c
sashakizim <kizimsk24@ukr.net>
2019-09-03 23:43:31 +0300

    logout done

--- WebGordon/ClientApp/src/components/auth/login/LogOut.js
+++ WebGordon/ClientApp/src/components/auth/login/LogOut.js
@@ -1,5 +1,56 @@
-﻿const LogOut = () => {
-    const DoLogOut = confirm("Ви бажаєте вийти?");
-     return alert(DoLogOut);
+﻿import React from 'react';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import { Redirect } from 'react-router';
+import { connect } from 'react-redux';
+import { disposeCurrentUser } from '../../../actions/authActions';
+class LogOut extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            modal: true,
+            redirect :false
+        };
+
+        this.toggle = this.toggle.bind(this);
+        this.logout = this.logout.bind(this);
+    }
+
+    toggle() {
+        this.setState(prevState => ({
+            modal: !prevState.modal
+        }));
+        
+    }
+
+    logout() {
+
+        this.setState(prevState => ({
+            modal: false,
+            redirect: true
+        }));
+        
+    
+       
+    }
+    render() {
+        
+            const modal=(
+                <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
+                    <ModalHeader toggle={this.toggle}>Modal title</ModalHeader>
+                    <ModalBody>
+                      Are you sure?
+          </ModalBody>
+                    <ModalFooter>
+                        <Button color="primary" onClick={(event) => { this.props.disposeCurrentUser(); this.logout() }}>Logout</Button>{' '}
+                        <Button color="secondary" onClick={this.toggle}>Cancel</Button>
+                    </ModalFooter>
+                </Modal>)
+
+        return (
+            this.state.redirect ?
+                <Redirect to="/" /> :
+                modal)
+    }
 }
-export default LogOut;
+
+export default connect(null, { disposeCurrentUser } )(LogOut);
