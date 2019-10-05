ca0cdf1793db2cc99b856ac34775348ff056b51a
svvrv <shvvrv@gmail.com>
2019-10-05 13:10:35 +0300

    lot-full in process

--- WebGordon/ClientApp/src/components/lot-full/Lot-full.jsx
+++ WebGordon/ClientApp/src/components/lot-full/Lot-full.jsx
@@ -3,95 +3,160 @@ import { Link } from 'react-router-dom';
 import { Button } from 'reactstrap';
 import classnames from 'classnames';
 import './Lot-full.css';
+import ImageGallery from 'react-image-gallery';
+import '../../../node_modules/react-image-gallery/styles/css/image-gallery.css';
 
 class LotFull extends Component {
+
+    state = {
+        torgId: "2",
+        productName: "Продам дрова",
+        quantity: "10",
+        dimensions: "т",
+        currentPrice: "50",
+        betCount: "12",
+        remainingTime: "2 часа (16:30 22.10.2019)",
+        sellerImg: null,
+        sellerName: "Найкращий продавець",
+        torgDescript: "Просьба покупателям выходить на связь в течение 3-х дней с конкретными предложениями по выкупу лота.Лот должен быть выкуплен в течение пяти дней после договоренности о механизмах передачи лота и денег.Если в течение 5 суток не получу конкретных предложений или вы не ответили на мое письмо, то на шестой день лот перевыставляется и претензии по продаже лота не принимаю, вам ставлю отрицательный отзыв.",
+        torgDelivery: "<h5>Способы оплаты:</h5>< span > По договоренности</span> <br /><span>Банковский перевод</span><h5>Доставка:</h5>< p > Почта России по городу: <span>300 руб.</span> по стране: < span > 300 руб.</span> по миру: <span>300 руб.</span> <br />Комментарий: <i>заказное письмо </i></p >",
+        productImages: [
+        {
+            original: 'https://picsum.photos/id/1018/1000/600/',
+            thumbnail: 'https://picsum.photos/id/1018/250/150/',
+        },
+        {
+            original: 'https://picsum.photos/id/1015/1000/600/',
+            thumbnail: 'https://picsum.photos/id/1015/250/150/',
+        },
+        {
+            original: 'https://picsum.photos/id/1019/1000/600/',
+            thumbnail: 'https://picsum.photos/id/1019/250/150/',
+        },
+        ],
+        yourPrice:  null,
+        errors: {}
+    }
+
+    handleChange = (e) => {
+        //if (isNaN(String.fromCharCode(e.target.keyCode))) return;
+        e.target.value = e.target.value.replace(/[^0-9]/g, "");
+        if (e.target.value <= this.state.currentPrice) {
+            let errors = { invalid: "Неправильно введено величину ставки!" };
+            this.setState({ errors })
+        }
+        this.setState(
+            { [e.target.name]: e.target.value })
+        //this.setStateByErrors(e.target.name, e.target.value);
+    }
+
+    setStateByErrors = (name, value) => {
+        if (!!this.state.errors[name]) {
+            let errors = Object.assign({}, this.state.errors);
+            delete errors[name];
+            this.setState(
+                {
+                    [name]: value,
+                    errors
+                })
+        }
+        else {
+            this.setState(
+                { [name]: value })
+        }
+    }
 
     render() {
         const errors = {};
         const isLoading = null;
-
-
+        const { productName, quantity, dimensions, currentPrice, betCount, remainingTime, sellerImg, sellerName, torgDescript, torgDelivery, productImages, yourPrice } = this.state;
         return (
             <React.Fragment>
-                <div class="row content border rounded-lg" >
-                    <div class="col-sm-12">
-                        <div class="col-sm-12" style={{ borderBottom: "1px solid #dfdfdf" }}>
-                            <div class="col-sm-7 xs_center_left_upwards">
-                                <span class="text">Назва лота Продам дрова</span>
+                <div className="row content border rounded-lg" >
+                    <div className="col-sm-12">
+                        <div className="col-sm-12" style={{ borderBottom: "1px solid #dfdfdf" }}>
+                            <div className="col-sm-7 xs_center_left_upwards">
+                                <h3 className="title" >{productName}</h3>
                             </div>
                         </div>
                     </div>
 
-                    <div class="col-xs-12 col-sm-6 col-md-6">
-                        <div class="border" >
-                            <img className="img-thumbnail border-0" src="https://cdn.auth0.com/blog/react-js/react.png" alt="lot" />
+                    <div className="col-xs-12 col-sm-6 col-md-6">
+                        <div className="border" >
+                            <ImageGallery items={productImages} />
                         </div>
                     </div>
-                    <div class="col-xs-12 col-sm-6 col-md-6">
-                        <div class="border-bottom">
-                            <h3 class="title" >Знову назва лота 2019 Армения 1000 драм Адмирал Исаков 125 лет серебро 1 унция в родной коробке с сертификатом</h3>
-                            <span class="">Количество: 1</span>
+                    <div className="col-xs-12 col-sm-6 col-md-6">
+                        <div className="border-bottom">
+                            <h3 className="title" >{productName}</h3>
+                            <span className="">Количество: {quantity} {dimensions}.</span>
                         </div>
-                        <div class="">
-                            <div class="row border-bottom" style={{ marginRight: "4px" }}>
-                                <div class="col-xs-6 col-sm-6 col-md-6">
+                        <div className="">
+                            <div className="row border-bottom" style={{ marginRight: "4px" }}>
+                                <div className="col-xs-6 col-sm-6 col-md-6">
                                     Актуальна ціна:
                             </div>
-                                <div class="col-xs-6 col-sm-6 col-md-6">
-                                    50 грн
+                                <div className="col-xs-6 col-sm-6 col-md-6">
+                                    {currentPrice} грн
                             </div >
 
                             </div >
-                            <div class="row border-bottom" style={{ marginRight: "4px" }}>
-                                <div class="col-xs-6 col-sm-6 col-md-6">
+                            <div className="row border-bottom" style={{ marginRight: "4px" }}>
+                                <div className="col-xs-6 col-sm-6 col-md-6">
                                     Кількість ставок:
                             </div>
-                                <div class="col-xs-6 col-sm-6 col-md-6">
-                                    12
+                                <div className="col-xs-6 col-sm-6 col-md-6">
+                                    {betCount}
                             </div >
-                                <div class="">
+                                <div className="">
                                     <form onSubmit={this.onSubmitForm}>
                                         <h2 style={{ textAlign: "center" }}></h2>
+
                                         {
                                             !!errors.invalid ?
                                                 <div className="alert alert-danger">
                                                     <strong>Помилка!</strong> {errors.invalid}.
                                         </div> : ''
                                         }
+
                                         <div className={classnames('form-group', { 'has-error': !!errors.name })}>
 
-                                            <label htmlFor="curprice">Ваша ставка</label>
+                                            {
+                                                !!errors.invalid ?
+                                                    <div className="alert alert-danger">
+                                                        <strong>Помилка!</strong> {errors.invalid}.
+                    </div> : ''
+                                            }
+
+                                            <label htmlFor="yourPrice">Ваша ставка</label>
                                             <input type="text"
                                                 className="form-control"
-                                                id="curprice"
-                                                name="curprice"
-                                                value={51}
+                                                id="yourPrice"
+                                                name="yourPrice"
+                                                onChange={this.handleChange}
+                                                value={yourPrice ? yourPrice :  1+parseInt(currentPrice) }
                                             />
 
                                             <div className="form-group">
                                                 <div className="">
                                                     <button type="submit"
                                                         className="btn btn-warning"
-                                                        style={{ width: 100 + '%' }}
+                                                        style={{ width: 100 + '%', marginTop: "3px" }}
                                                         disabled={isLoading}>Зробити ставку <i className="fa fa-check-circle" aria-hidden="true"></i></button>
                                                 </div>
                                             </div>
-                                        </div>
+                                        </div> 
                                     </form>
                                 </div >
                                 <div class="row">
-                                    Завершение: 2 часа (02.10.2019 16:00)
+                                    Завершение: {remainingTime}
                             </div >
                                 <div class="row">
                                     Продавець:
-                                <img className="img-thumbnail" style={{ width: "40px" }} src="https://cdn.auth0.com/blog/react-js/react.png" alt="Userimage" />
-                                    ЮзерАдмін
+                                <img className="img-thumbnail" style={{ width: "40px" }} src={sellerImg ? sellerImg : "https://cdn.auth0.com/blog/react-js/react.png"} alt="Userimage" />
+                                    {sellerName}
                             </div >
                             </div >
-                            <div class="row border-bottom" style={{ marginRight: "4px" }}>
-                                Стоимость доставки оплачивает: Покупатель
-                        </div>
-
                         </div>
                     </div >
                 </div >
@@ -99,49 +164,30 @@ class LotFull extends Component {
 
 
 
-                <div class="row content border rounded-lg" >
+                <div className="row content border rounded-lg" >
                     <div>
-                        <ul class="nav nav-tabs " id="myTab" role="tablist">
-                        <li class="nav-item">
-                            <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Описание</a>
+                        <ul className="nav nav-tabs " id="myTab" role="tablist">
+                        <li className="nav-item">
+                                <a className="nav-link active" id="descr-tab" data-toggle="tab" href="#descr" role="tab" aria-controls="descr" aria-selected="true">Описание</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Оплата и доставка</a>
+                        <li className="nav-item">
+                                <a className="nav-link" id="deliv-tab" data-toggle="tab" href="#deliv" role="tab" aria-controls="deliv" aria-selected="false">Оплата и доставка</a>
                         </li>
 
                     </ul>
-                        <div class="tab-content " id="myTabContent">
-                        <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+                        <div className="tab-content " id="myTabContent">
+                            <div className="tab-pane fade show active" id="descr" role="tabpanel" aria-labelledby="descr-tab">
                             <h5>стандартное описание:</h5>
                             <div>
-                                <h5><strong>Просьба покупателям выходить на связь в течение 3-х
-                                дней с конкретными предложениями по выкупу лота. Лот должен быть
-                                выкуплен в течение пяти дней после договоренности о механизмах
-                                передачи лота и денег. Если в течение 5 суток не получу конкретных
-                                предложений или вы не ответили на мое письмо, то на шестой день лот
-                                перевыставляется и претензии по продаже лота не принимаю, вам
-                                    ставлю отрицательный отзыв.</strong></h5>
-                                <h5>
-                                    <strong>Лоты</strong>&nbsp;объединяются&nbsp;в&nbsp;одну&nbsp;посылку!!</h5>
-                                <h5><strong>Со своей стороны обязуюсь высылать лот в течение
-                                    максимум пяти дней со дня отправки вами денег</strong></h5>
-
+                                    {torgDescript}
                             </div>
                         </div>
-                        <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+                            <div className="tab-pane fade" id="deliv" role="tabpanel" aria-labelledby="deliv-tab">
                             <div>
                                     <h5>Тип сделки:</h5>
                                 <p>Предоплата</p>
                             </div>
-                            <div>
-                                <h5>Способы оплаты:</h5>
-                                    <span>По договоренности</span><br/>
-                                    <span>Банковский перевод</span>
-                            </div>
-                            <div>
-                                <h5>Доставка:</h5>
-                                <p>Почта России по городу: <span>300 руб.</span> по стране: <span>300 руб.</span> по миру:  <span>300 руб.</span><br />Комментарий: <i>заказное письмо </i></p>
-                            </div>
+                                {torgDelivery}
                         </div>
                         </div>
                     </div>
@@ -169,3 +215,4 @@ class LotFull extends Component {
 }
 
 export default LotFull
+                           // <img className="img-thumbnail border-0" src="https://cdn.auth0.com/blog/react-js/react.png" alt="lot" />
