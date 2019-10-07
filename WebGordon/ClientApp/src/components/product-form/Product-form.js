b4699bc069d0d27f98cf6ac2a2a568416f3f9f7b
svvrv <shvvrv@gmail.com>
2019-10-07 15:17:08 +0300

    product form in process

--- WebGordon/ClientApp/src/components/product-form/Product-form.js
+++ WebGordon/ClientApp/src/components/product-form/Product-form.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import classnames from 'classnames';
 import { Redirect } from 'react-router';
-import { register } from '../../actions/authActions'
-import { connect } from "react-redux";
+//import { register } from '../../actions/authActions'
+//import { connect } from "react-redux";
 import PropTypes from 'prop-types';
 
 import ImgItem from './ImgItem'
@@ -20,12 +20,18 @@ class ProductForm extends Component {
         errors: {},
         isLoading: false,
         done: false,
+        disabled: false,
 
         image: ''
     }
 
     handleChange = (e) => {
 
+        this.setStateByErrors(e.target.name, e.target.value);
+    }
+
+    handleDigitChange = (e) => {
+        e.target.value = e.target.value.replace(/[^0-9]/g, "");
         this.setStateByErrors(e.target.name, e.target.value);
     }
 
@@ -36,6 +42,30 @@ class ProductForm extends Component {
                 let reader = new FileReader();
                 reader.onload = (e) => {
                     this.setStateByErrors(name, e.target.result);
+                }
+                reader.readAsDataURL(files[0]);
+            }
+            else alert('Обраний файл не є зображенням. Спробуйте ще раз')
+        }
+    }
+
+    handleImageChange2 = (evt) => {
+        const { name, files } = evt.target;
+        if (files && files[0]) {
+            if (files[0].type.match(/^image\//)) {
+                let reader = new FileReader();
+                reader.onload = (e) => {
+
+                    const newImage={
+                        id: this.state.images.count,
+                        main: false,
+                        image: e.target.result
+                    }
+                    this.setState((prevstate) => {
+                        const newImages = [...this.state.images, newImage];
+                        return ({ images: newImages })
+                    });
+                    //this.setStateByErrors(name, e.target.result);
                 }
                 reader.readAsDataURL(files[0]);
             }
@@ -64,6 +94,9 @@ class ProductForm extends Component {
         let errors = {};
         if (this.state.productName === '') errors.productName = "Поле є обов'язковим!";
         if (this.state.quantity === '') errors.quantity = "Поле є обов'язковим!"
+        if (this.state.dimensions === 'Виберіть...' || this.state.dimensions === '') errors.dimensions = "Поле є обов'язковим!"
+        if (this.state.startPrice === '') errors.startPrice = "Поле є обов'язковим!"
+        if (this.state.torgTime === '') errors.torgTime = "Поле є обов'язковим!"
 
 
         const isValid = Object.keys(errors).length === 0
@@ -71,11 +104,11 @@ class ProductForm extends Component {
             const { name, email, telnumber, password, description, image } = this.state;
             this.setState({ isLoading: true });
             console.log('---SEND DATA state----', { Name: name, Telnumber: telnumber, Password: password, Description: description, Image: image });
-            this.props.register({ Name: name, Telnumber: telnumber, Email: email, Password: password, Description: description, Image: image })
-                .then(
-                    () => this.setState({ done: true }),
-                    (err) => this.setState({ errors: err.response.data, isLoading: false })
-                );
+        //    this.props.register({ Name: name, Telnumber: telnumber, Email: email, Password: password, Description: description, Image: image })
+        //        .then(
+        //            () => this.setState({ done: true }),
+        //            (err) => this.setState({ errors: err.response.data, isLoading: false })
+        //        );
         }
         else {
             this.setState({ errors });
@@ -85,7 +118,7 @@ class ProductForm extends Component {
 
 
     render() {
-        const errors = {};
+        const { errors } = this.state;
         const image = this.state.image;
         const isLoading = false;
         console.log("---render form--", this.state)
@@ -102,10 +135,10 @@ class ProductForm extends Component {
                         </div> : ''
                 }
 
-                <div className={classnames('form-group', { 'is-invalid': !!errors.productName })}>
+                <div className='form-group'>
                     <label htmlFor="productName">Назва товару</label>
                     <input type="text"
-                        className="form-control"
+                        className={classnames('form-control', { 'is-invalid': !!errors.productName })}
                         id="productName"
                         name="productName"
                         value={this.state.productName}
@@ -114,43 +147,44 @@ class ProductForm extends Component {
                 </div>
 
                 <div class="form-row">
-                    <div className={classnames('form-group col-md-8', { 'is-invalid': !!errors.quantity })}>
+                    <div className='form-group col-md-8'>
                         <label htmlFor="quantity">Кількіть товару для продажу</label>
                         <input type="text"
-                            className="form-control"
+                            className={classnames('form-control', { 'is-invalid': !!errors.quantity })}
                             id="quantity"
                             name="quantity"
                             value={this.state.quantity}
-                            onChange={this.handleChange} />
+                            onChange={this.handleDigitChange} />
                         {!!errors.quantity ? <span className="text-muted help-block">{errors.quantity}</span> : ''}
                     </div>
-                    <div class="form-group col-md-4">
+                    <div class='form-group col-md-4'>
                         <label for="dimensions">Одиниці виміру</label>
-                        <select id="dimensions" class="form-control">
-                            <option selected>Виберіть...</option>
+                        <select name="dimensions" onChange={this.handleChange} id="dimensions" class={classnames('form-control', { 'is-invalid': !!errors.dimensions })}>
+                            <option selected>{this.state.dimensions}</option>
                             <option>шт.</option>
                             <option>м.куб.</option>
                             <option>т.</option>
                             <option>кг.</option>
-                        </select>
+                         </select>
+                        {!!errors.dimensions ? <span className="text-muted help-block">{errors.dimensions}</span> : ''}
                     </div>
                 </div>
 
-                <div className={classnames('form-group', { 'is-invalid': !!errors.startPrice })}>
+                <div className='form-group'>
                     <label htmlFor="startPrice">Стартова ціна, грн</label>
                     <input type="text"
-                        className="form-control"
+                        className={classnames('form-control', { 'is-invalid': !!errors.startPrice })}
                         id="startPrice"
                         name="startPrice"
                         value={this.state.startPrice}
-                        onChange={this.handleChange} />
+                        onChange={this.handleDigitChange} />
                     <small id="startPriceHelpBlock" class="form-text text-muted">Ціна, з якої будуть починатися торги</small>
                     {!!errors.startPrice ? <span className="text-muted help-block">{errors.startPrice}</span> : ''}
                 </div>
 
                 <div class="form-group">
                     <label for="torgTime">Час проведення аукціону</label>
-                    <select id="torgTime" class="form-control">
+                    <select id="torgTime" onChange={this.handleChange} class={classnames('form-control', { 'is-invalid': !!errors.torgTime })}>
                         <option selected>Виберіть...</option>
                         <option>3 дні</option>
                         <option>1 тиждень</option>
@@ -158,6 +192,7 @@ class ProductForm extends Component {
                         <option>3 тижні</option>
                     </select>
                     <small id="torgTimeHelpBlock" class="form-text text-muted">Бажаний час проведення торгів</small>
+                    {!!errors.torgTime ? <span className="text-muted help-block">{errors.torgTime}</span> : ''}
                 </div>
 
                 <div className='form-group'>
@@ -199,7 +234,7 @@ class ProductForm extends Component {
                         className="form-control-file"
                         id="image"
                         name="image"
-                        onChange={this.handleImageChange} />
+                        onChange={this.handleImageChange2} />
                 </div>
 
                 {
